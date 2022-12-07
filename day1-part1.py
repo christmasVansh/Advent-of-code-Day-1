97535edcfabf305fcf1bff39f951bcda0cd600fc
christmasVansh <120047915+christmasVansh@users.noreply.github.com>
2022-12-07 18:00:38 +0000

    Create day1-part1.py

---
+++ day1-part1.py
@@ -0,0 +1,17 @@
+with open('input.txt', 'r') as calorie:
+    lines = calorie.readlines()
+    calories = [entry.strip() for entry in lines]
+
+sums = []
+current_sum = 0
+for entry in calories:
+    if entry != '':
+        current_sum += int(entry)
+    elif entry == '':
+        sums.append(current_sum)
+        current_sum = 0
+sums.append(current_sum)
+
+print(max(sums))
+
+#My answer was 200945
